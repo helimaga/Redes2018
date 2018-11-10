7315d7f321e6f678c24fe8421c8ada7948770018
helimaga <helimga@gmail.com>
2018-11-09 21:36:58 -0300

    Avances en el pairwise alignment, ahora funciona para las dos matrices blosum62 y pMHCmatrix. Papers relevantes

--- TPfinal/epitopes.py
+++ TPfinal/epitopes.py
@@ -17,6 +17,15 @@ import numpy as np
 from Bio import pairwise2
 from Bio.SubsMat.MatrixInfo import blosum62
 import matplotlib.patches as mpatches
+import csv
+
+#%%
+
+#paths
+
+pathHeli = '/home/heli/Documents/Redes/Practicas/TPs/Redes2018/TPfinal/'
+#pathJuan
+#pathSanti
 
 #%%
 
@@ -24,24 +33,24 @@ import matplotlib.patches as mpatches
 #filtro por las secuencias con Score mayor a 0 (con algun grado de confianza para su anotacion), Gene igual a TRB == T-cell Receptor Beta, Species igual a Homo Sapiens y MHC_class (clase del Complejo Mayor de Histocompatibilidad) igual a I. 
 #me quedo con las columnas CDR3 (Complementary Determining Region 3) que corresponde a la secuencia de aminoacidos del TRB, Epitope que corresponde a la secuencia del epitope y Epitope_gene que corresponde al gen del cual proviene dicho epitope.
 
-path = '/home/heli/Documents/DataEpitopes/'
+path = pathHeli
 
 virusStr = ['YFV', 'CMV', 'EBV', 'HIV-1', 'InfAV', 'HCV']
-epitopes = {}
+TRBdf = {}
 
 for i in virusStr:    
     df0 = pd.read_csv(path + i, sep='\t')
     df1 = df0[(df0.Score > 0) &(df0.Gene == 'TRB') & (df0.Species == 'HomoSapiens') & (df0.MHC_class == 'MHCI')]
     df2 = df1 [['CDR3', 'Epitope', 'Epitope_gene', 'Epitope_species']]
     df3 = df2.drop_duplicates(subset = 'CDR3')
-    epitopes[i] = df3
+    TRBdf[i] = df3
 
 #%%% 
     
 #prueba de pairwise2 de Biopython
 #pairwise sequence alignment
 
-alignments = pairwise2.align.localds("LSPADKTNVKAA", "PEEKSAV", blosum62, -10, -1)
+alignments = pairwise2.align.localds("LSPADKTNVKAA", "PEEKAVA", pMHCmatrix, -10, -1)
 
 #local -> uses Smith Waterman local alignment algorithm
 #match parameter 'd': A dictionary returns the score of any pair of characters -> BLOSUM62
@@ -56,29 +65,77 @@ alignments = pairwise2.align.localds("LSPADKTNVKAA", "PEEKSAV", blosum62, -10, -
 #tomo 145 TRB de cada categoria (virus) de forma random 
 #genero un dataframe conjunto con 1200 TRB
 
-epitopes_rnd0 = []
+TRBdf_rnd0 = []
 
 for i in virusStr:
-    epitopes_rnd0.append(epitopes[i].sample(n=145))
+    TRBdf_rnd0.append(TRBdf[i].sample(n=145))
 
-epitopes_rnd = pd.concat(epitopes_rnd0)
+TRBdf_rnd = pd.concat(TRBdf_rnd0)
+TRBdf_rnd = TRBdf_rnd.drop_duplicates(subset = 'CDR3')
 
-epitopes_rnd = epitopes_rnd.drop_duplicates(subset = 'CDR3')
 #hay un solo CDR3 que esta repetido, ver!
+TRBdf_rnd = TRBdf_rnd.reset_index(drop=True)
 
-epitopes_rnd = epitopes_rnd.reset_index(drop=True)
+#%%
 
-#tomo todos los pares unicos de secuencias y hago pairwise alignment de las mismas para obtener una score que refleje la similud de secuencia
+#cargo la matriz de similaridad del paper de Kim et al. (2009) (novelmatrixTCR.pdf) 
 
-CDR3_pairs = itr.combinations(epitopes_rnd["CDR3"], 2)
-seq_scores = []
-selected_CDR3_pairs = []
+peptideMHCmatrix = pd.read_csv(path + 'newmatrix.txt', sep=' ')
+peptideMHCmatrix.columns = (list(peptideMHCmatrix)[1:21]+[0])
+peptideMHCmatrix = peptideMHCmatrix.iloc[:,0:20]
 
-for (seq1, seq2) in CDR3_pairs:
-    score = pairwise2.align.localds(seq1, seq2, blosum62, -10, -1, score_only=True)
-    seq_scores.append(score)
-    if score > 45:
-        selected_CDR3_pairs.append((seq1, seq2, {'weight': score}))
+#para reescalear todos los valores de la matriz (dataframe)
+#peptideMHCmatrix = peptideMHCmatrix.multiply(1000).astype(int)
+
+pMHCmatrix = {}
+
+pairsmatrix = list(itr.combinations_with_replacement(list(peptideMHCmatrix),2))
+for (i,j) in pairsmatrix:
+    pMHCmatrix[(i,j)] = peptideMHCmatrix[i][j]
+
+
+#%%
+
+#falta terminar ojo, no correr este chunk!
+#escribo un archivo .csv de los TRB CDR3 (para luego calcular la kernel similarity measure)
+    
+TRB_CDR3 = list(TRBdf_rnd['CDR3'])
+
+path_TRB_CDR3 = '/home/heli/Documents/Lab Immunoinformatics/epitope_CDR3.csv'
+
+with open(path_TRB_CDR3, mode='w') as TRB_file:
+    TRB_writer = csv.writer(TRB_file, delimiter='\n')
+    TRB_writer.writerow(TRB_CDR3)
+
+#%%
+
+#tomo todos los pares unicos de secuencias y hago pairwise alignment de las 
+#mismas para obtener una score que refleje la similud de secuencia con las dos matrices
+#Blosum 62 y pMHCmatrix
+
+CDR3_pairs = list(itr.combinations(TRBdf_rnd["CDR3"], 2))
+
+seq_scores = {}
+selected_CDR3_pairs = {}
+
+matrices = {'blosum62':blosum62, 'pMHCmatrix':pMHCmatrix}
+gap_open_penalty = {'blosum62':-10, 'pMHCmatrix':-1}
+gap_extension_penalty = {'blosum62':-1, 'pMHCmatrix':-0.1}
+threshold = {'blosum62':45, 'pMHCmatrix':2}
+
+for i in matrices.keys():
+    scorelist = []
+    selected_CDR3_list = []
+    for (seq1, seq2) in CDR3_pairs:
+        score = pairwise2.align.localds(seq1, seq2, matrices[i], gap_open_penalty[i], gap_extension_penalty[i], score_only=True)
+        scorelist.append(score)
+        if score > threshold[i]:
+            selected_CDR3_list.append((seq1, seq2, {'weight': score}))
+    selected_CDR3_pairs[i] = selected_CDR3_list
+    seq_scores[i] = scorelist
+    print(i)
+    
+#para encontrar el cutoff o threshold adecuado, ver como estan distribuidos los scores
 
 max(seq_scores)
 min(seq_scores)
@@ -87,7 +144,7 @@ np.mean(seq_scores)
 np.std(seq_scores)
 
 #hay que evaluar adonde poner el cutoff para el score calculado
-#por el momento lo fije en 45 que es mas que media + 1 desvio std
+#por el momento lo fije en 45 que es mas que media + 1 desvio std (Blosum62)
 
 #hago un histograma con los scores de las secuencias
 
@@ -98,25 +155,31 @@ plt.tick_params(axis='both', which='major', labelsize=10)
 plt.xlabel('Sequence score', fontsize=20)
 plt.show()
 
+#%%
 #armo el grafo con el output del pairwise alignment de secuencias
+#le agrego el atrobuto a los nodos de epitope_sp es decir a que virus corresponde el epitopes al cual se une la secuencia CDR3
 
-TRB = nx.Graph()
-TRB.add_edges_from(selected_CDR3_pairs)
+TRB = {}
 
-for n in epitopes_rnd['CDR3']:
-    if n in list(TRB.nodes()):
-        TRB.nodes[n]['epitope_sp'] = epitopes_rnd['Epitope_species'][epitopes_rnd['CDR3'] == n].to_string(index=False)
-    else:
-        TRB.add_node(n, epitope_sp = epitopes_rnd['Epitope_species'][epitopes_rnd['CDR3'] == n].to_string(index=False))
+for i in matrices:
+    TRB[i] = nx.Graph()
+    TRB[i].add_edges_from(selected_CDR3_pairs[i])
+    for n in TRBdf_rnd['CDR3']:
+        if n in list(TRB[i].nodes()):
+            TRB[i].nodes[n]['epitope_sp'] = TRBdf_rnd['Epitope_species'][TRBdf_rnd['CDR3'] == n].to_string(index=False)
+    
+
+TRB[i].number_of_nodes()
+TRB[i].number_of_edges()
+
+#los nodos que quedaron no enlazados los descartamos
+
+#    else:
+#        TRBdf.add_node(n, epitope_sp = TRBdf_rnd['Epitope_species'][TRBdf_rnd['CDR3'] == n].to_string(index=False))
 
 
-#len(np.unique(epitopes_rnd['CDR3']))
-#len(epitopes_rnd['CDR3'])
-
-TRB.number_of_nodes()
-TRB.number_of_edges()
-
-netDeg = np.array(list(TRB.degree()))
+#tambien es interesante evaluar la distribucion de grado de los nodos para distintos thresholds o cutoffs
+netDeg = np.array(list(TRB[i].degree()))
 CDR3_netDeg = [int(x) for x in netDeg[:,1]]
 
 np.median(CDR3_netDeg)
@@ -130,30 +193,38 @@ plt.tick_params(axis='both', which='major', labelsize=10)
 plt.xlabel('Selected sequence degree', fontsize=20)
 plt.show()
 
+#%%
+
+#plots de los grafos TRB constriudos
+
 # use one of the edge properties to control line thickness
-edgewidth = [ d['weight']/20 for (u,v,d) in TRB.edges(data=True)]
+edgewidth = [ d['weight']/20 for (u,v,d) in TRB[i].edges(data=True)]
 
 plt.figure()
-nx.draw(TRB, width = edgewidth, node_size=80, font_size=20)
+nx.draw(TRB[i], width = edgewidth, node_size=80, font_size=20)
 plt.suptitle('TRB sequence similarity network', fontsize=20)
 plt.show()
 
+###
+
 color_code = {'YellowFeverVirus':'m', 'CMV':'g', 'HIV-1':'k',  'EBV':'b', 'InfluenzaA':'r', 'HCV':'c'}
 
-TRB_epitopes = list(nx.get_node_attributes(TRB, "epitope_sp").values())
+TRB_node_color = {}
+pos = {}
 
-TRB_node_color = []
-for i in TRB_epitopes:
-    TRB_node_color.append(color_code[i])
-
-
-pos = nx.kamada_kawai_layout(TRB)
+for i in matrices:
+    TRB_epitopes = list(nx.get_node_attributes(TRB[i], "epitope_sp").values())
+    node_color = []
+    for k in TRB_epitopes:
+        node_color.append(color_code[k])
+    TRB_node_color[i] = node_color
+    pos[i] = nx.kamada_kawai_layout(TRB[i])
 
 plt.figure()
-nx.draw(TRB,
+nx.draw(TRB[i],
         width=0.1,
         edge_color = 'k',
-        node_color = TRB_node_color, 
+        node_color = TRB_node_color[i], 
         node_size=50,
         font_size=10,
         with_labels=False,
@@ -161,11 +232,11 @@ nx.draw(TRB,
 plt.show()
 
 plt.figure()
-nx.draw(TRB,
-        pos,
+nx.draw(TRB[i],
+        pos[i],
         width=0.1,
         edge_color = 'k',
-        node_color = TRB_node_color, 
+        node_color = TRB_node_color[i], 
         node_size=50,
         font_size=10,
         with_labels=False,
@@ -178,6 +249,18 @@ InfluenzaA = mpatches.Patch(color='r', label='Influenza A Virus')
 HCV = mpatches.Patch(color='c', label='Hepatitis C Virus')
 plt.legend(handles=[YFV, CMV, HIV,  EBV, InfluenzaA, HCV], fontsize=20)
 plt.show()
+
+
+#%%%
+
+
+
+
+
+
+
+
+
 
 
 
